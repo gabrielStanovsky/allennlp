a9fcd54e0d06d934a8c17bd47b477e251f88a4fc
Gabi Stanovsky <gabis@julianm.local>
2018-07-05 15:51:00 -0700

    minor

---
+++ tests/models/open_ie/open_ie_test.py
@@ -0,0 +1,15 @@
+# pylint: disable=no-self-use,invalid-name
+
+import torch
+from torch.autograd import Variable
+from allennlp.common.testing import ModelTestCase
+
+
+class OpenIETest(ModelTestCase):
+    def setUp(self):
+        super(OpenIETest, self).setUp()
+        self.set_up_model('tests/fixtures/coref/experiment.json',
+                          'tests/fixtures/coref/coref.gold_conll')
+
+    def test_coref_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
