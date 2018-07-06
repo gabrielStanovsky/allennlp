ba5d6a6505a8789402509ea0b3340839d00f3ba5
Gabi Stanovsky <gabis@julianm.local>
2018-07-06 11:22:55 -0700

    model wip

--- tests/service/predictors/openie_predictor_test.py
+++ tests/service/predictors/openie_predictor_test.py
@@ -3,8 +3,6 @@ from unittest import TestCase
 
 from allennlp.models.archival import load_archive
 from allennlp.service.predictors import Predictor
-from processes.models.propara_baseline_model import ProParaBaselineModel
-from processes.service.predictors.propara_prediction import ProParaPredictor
 
 
 class TestOpenIEPredictor(TestCase):
@@ -12,9 +10,9 @@ class TestOpenIEPredictor(TestCase):
     def test_sentence_inputs_with_multiple_predicates(self):
         # sentence_tokens
         inputs = {"sentence_tokens": "Angela Merkel met and spoke to her EU counterparts during the climate submit in Paris."}
-        archive = load_archive('tests/fixtures/openie_models/toy_model.tar.gz')
+        archive = load_archive('tests/fixtures/openie/toy_model.tar.gz')
         predictor = Predictor.from_archive(archive, 'openie_predictor')
         result = predictor.predict_json(inputs)
         # one each for met and spoke.
         # FIXME add an example from dev set to match performance with non-AllenNLP implementation.
-        assert(len(result['outputs'] == 2))
+        assert(len(result['outputs']) == 2)
