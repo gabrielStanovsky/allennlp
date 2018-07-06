9a23f99bdb9a1076f54273e848f7664863a0bc40
Gabi Stanovsky <gabis@julianm.local>
2018-07-06 10:49:24 -0700

    model wip

--- tests/models/open_ie/open_ie_test.py
+++ tests/models/open_ie/open_ie_test.py
@@ -11,5 +11,5 @@ class OpenIETest(ModelTestCase):
         self.set_up_model('tests/fixtures/openie/experiment.json',
                           'tests/fixtures/openie/open_ie.gold_conll')
 
-    def test_coref_model_can_train_save_and_load(self):
+    def test_openie_model_can_train_save_and_load(self):
         self.ensure_model_can_train_save_and_load(self.param_file)
