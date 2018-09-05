04b34db32537d2d44809585f910622d4d1f2f474
Gabi Stanovsky <gabis@julianm.local>
2018-09-05 13:34:21 -0700

    cleaning

---
+++ allennlp/models/open_information_extraction.py
@@ -0,0 +1,212 @@
+from typing import Dict, List, TextIO, Optional
+
+from overrides import overrides
+import torch
+from torch.nn.modules import Linear, Dropout
+import torch.nn.functional as F
+
+from allennlp.common import Params
+from allennlp.common.checks import check_dimensions_match
+from allennlp.data import Vocabulary
+from allennlp.modules import Seq2SeqEncoder, TimeDistributed, TextFieldEmbedder
+from allennlp.modules.token_embedders import Embedding
+from allennlp.models.model import Model
+from allennlp.nn import InitializerApplicator, RegularizerApplicator
+from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
+from allennlp.nn.util import get_lengths_from_binary_sequence_mask, viterbi_decode
+from allennlp.training.metrics import SpanBasedF1Measure
+
+
+@Model.register("openie")
+class NeuralOIE(Model):
+    """
+    This model performs Open Information Extraction (Open IE) using BIO tags representing predicate and
+    arbitrary number of arguments.
+    Specifically, it is an implementation of `Supervised Open Information
+    Extraction <http://www.aclweb.org/anthology/N18-1081>`_ .
+
+    This implementation is effectively a series of stacked interleaved LSTMs with highway
+    connections, applied to embedded sequences of words concatenated with an indicator
+    whether or not a word is the verbal predicate to generate predictions for in
+    the sentence. Additionally, during inference, Viterbi decoding is applied to constrain
+    the predictions to contain valid BIO sequences.
+
+    The end result is very similar to the semantic role labeling model, differing
+    mainly in the predicate identification feature.
+
+    Parameters
+    ----------
+    vocab : ``Vocabulary``, required
+        A Vocabulary, required in order to compute sizes for input/output projections.
+    text_field_embedder : ``TextFieldEmbedder``, required
+        Used to embed the ``tokens`` ``TextField`` we get as input to the model.
+    encoder : ``Seq2SeqEncoder``
+        The encoder (with its own internal stacking) that we will use in between embedding tokens
+        and predicting output tags.
+    initializer : ``InitializerApplicator``, optional (default=``InitializerApplicator()``)
+        Used to initialize the model parameters.
+    regularizer : ``RegularizerApplicator``, optional (default=``None``)
+        If provided, will be used to calculate the regularization penalty during training.
+    label_smoothing : ``float``, optional (default = 0.0)
+        Whether or not to use label smoothing on the labels when computing cross entropy loss.
+    """
+    def __init__(self, vocab: Vocabulary,
+                 text_field_embedder: TextFieldEmbedder,
+                 encoder: Seq2SeqEncoder,
+                 embedding_dropout: float = 0.0,
+                 initializer: InitializerApplicator = InitializerApplicator(),
+                 regularizer: Optional[RegularizerApplicator] = None,
+                 label_smoothing: float = None) -> None:
+        super(NeuralOIE, self).__init__(vocab, regularizer)
+
+        self.text_field_embedder = text_field_embedder
+        self.num_classes = self.vocab.get_vocab_size("labels")
+
+        self.encoder = encoder
+        # There are exactly 2 binary features for the verb predicate embedding.
+        self.tag_projection_layer = TimeDistributed(Linear(self.encoder.get_output_dim(),
+                                                           self.num_classes))
+        self.embedding_dropout = Dropout(p=embedding_dropout)
+        self._label_smoothing = label_smoothing
+
+        check_dimensions_match(text_field_embedder.get_output_dim(),
+                               encoder.get_input_dim(),
+                               "text embedding dim + verb indicator embedding dim",
+                               "encoder input dim")
+        initializer(self)
+
+    def forward(self,  # type: ignore
+                tokens: Dict[str, torch.LongTensor],
+                pred_id: torch.LongTensor,
+                tags: torch.LongTensor = None) -> Dict[str, torch.Tensor]:
+        # pylint: disable=arguments-differ
+        """
+        Parameters
+        ----------
+        tokens : Dict[str, torch.LongTensor], required
+            The output of ``TextField.as_array()``, which should typically be passed directly to a
+            ``TextFieldEmbedder``. This output is a dictionary mapping keys to ``TokenIndexer``
+            tensors.  At its most basic, using a ``SingleIdTokenIndexer`` this is: ``{"tokens":
+            Tensor(batch_size, num_tokens)}``. This dictionary will have the same keys as were used
+            for the ``TokenIndexers`` when you created the ``TextField`` representing your
+            sequence.  The dictionary is designed to be passed directly to a ``TextFieldEmbedder``,
+            which knows how to combine different word representations into a single vector per
+            token in your input.
+        verb_indicator: torch.LongTensor, required.
+            An integer ``SequenceFeatureField`` representation of the position of the verb
+            in the sentence. This should have shape (batch_size, num_tokens) and importantly, can be
+            all zeros, in the case that the sentence has no verbal predicate.
+        tags : torch.LongTensor, optional (default = None)
+            A torch tensor representing the sequence of integer gold class labels
+            of shape ``(batch_size, num_tokens)``
+
+        Returns
+        -------
+        An output dictionary consisting of:
+        logits : torch.FloatTensor
+            A tensor of shape ``(batch_size, num_tokens, tag_vocab_size)`` representing
+            unnormalised log probabilities of the tag classes.
+        class_probabilities : torch.FloatTensor
+            A tensor of shape ``(batch_size, num_tokens, tag_vocab_size)`` representing
+            a distribution of the tag classes per word.
+        loss : torch.FloatTensor, optional
+            A scalar loss to be optimised.
+        """
+        embedded_text_input = self.embedding_dropout(self.text_field_embedder(tokens))
+        mask = get_text_field_mask(tokens)
+        batch_size, sequence_length, _ = embedded_text_input.size()
+
+        encoded_text = self.encoder(embedded_text_input, mask)
+
+        logits = self.tag_projection_layer(encoded_text)
+        reshaped_log_probs = logits.view(-1, self.num_classes)
+        class_probabilities = F.softmax(reshaped_log_probs, dim=-1).view([batch_size,
+                                                                          sequence_length,
+                                                                          self.num_classes])
+        output_dict = {"logits": logits, "class_probabilities": class_probabilities}
+        if tags is not None:
+            loss = sequence_cross_entropy_with_logits(logits,
+                                                      tags,
+                                                      mask,
+                                                      label_smoothing=self._label_smoothing)
+            self.span_metric(class_probabilities, tags, mask)
+            output_dict["loss"] = loss
+
+        # We need to retain the mask in the output dictionary
+        # so that we can crop the sequences to remove padding
+        # when we do viterbi inference in self.decode.
+        output_dict["mask"] = mask
+        return output_dict
+
+    @overrides
+    def decode(self, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        """
+        Does constrained viterbi decoding on class probabilities output in :func:`forward`.  The
+        constraint simply specifies that the output tags must be a valid BIO sequence.  We add a
+        ``"tags"`` key to the dictionary with the result.
+        """
+        all_predictions = output_dict['class_probabilities']
+        sequence_lengths = get_lengths_from_binary_sequence_mask(output_dict["mask"]).data.tolist()
+
+        if all_predictions.dim() == 3:
+            predictions_list = [all_predictions[i].data.cpu() for i in range(all_predictions.size(0))]
+        else:
+            predictions_list = [all_predictions]
+        all_tags = []
+        transition_matrix = self.get_viterbi_pairwise_potentials()
+        for predictions, length in zip(predictions_list, sequence_lengths):
+            max_likelihood_sequence, _ = viterbi_decode(predictions[:length], transition_matrix)
+            tags = [self.vocab.get_token_from_index(x, namespace="labels")
+                    for x in max_likelihood_sequence]
+            all_tags.append(tags)
+        output_dict['tags'] = all_tags
+        return output_dict
+
+    def get_metrics(self, reset: bool = False):
+        metric_dict = self.span_metric.get_metric(reset=reset)
+        # This can be a lot of metrics, as there are 3 per class.
+        # we only really care about the overall metrics, so we filter for them here.
+        return {x: y for x, y in metric_dict.items() if "overall" in x}
+
+    def get_viterbi_pairwise_potentials(self):
+        """
+        Generate a matrix of pairwise transition potentials for the BIO labels.
+        The only constraint implemented here is that I-XXX labels must be preceded
+        by either an identical I-XXX tag or a B-XXX tag. In order to achieve this
+        constraint, pairs of labels which do not satisfy this constraint have a
+        pairwise potential of -inf.
+
+        Returns
+        -------
+        transition_matrix : torch.Tensor
+            A (num_labels, num_labels) matrix of pairwise potentials.
+        """
+        all_labels = self.vocab.get_index_to_token_vocabulary("labels")
+        num_labels = len(all_labels)
+        transition_matrix = torch.zeros([num_labels, num_labels])
+
+        for i, previous_label in all_labels.items():
+            for j, label in all_labels.items():
+                # I labels can only be preceded by themselves or
+                # their corresponding B tag.
+                if i != j and label[0] == 'I' and not previous_label == 'B' + label[1:]:
+                    transition_matrix[i, j] = float("-inf")
+        return transition_matrix
+
+    @classmethod
+    def from_params(cls, vocab: Vocabulary, params: Params) -> 'NeuralOIE':
+        embedder_params = params.pop("text_field_embedder")
+        text_field_embedder = TextFieldEmbedder.from_params(vocab, embedder_params)
+
+        encoder = Seq2SeqEncoder.from_params(params.pop("encoder"))
+        label_smoothing = params.pop_float("label_smoothing", None)
+
+        initializer = InitializerApplicator.from_params(params.pop('initializer', []))
+        regularizer = RegularizerApplicator.from_params(params.pop('regularizer', []))
+        params.assert_empty(cls.__name__)
+        return cls(vocab=vocab,
+                   text_field_embedder=text_field_embedder,
+                   encoder=encoder,
+                   initializer=initializer,
+                   regularizer=regularizer,
+                   label_smoothing=label_smoothing)
