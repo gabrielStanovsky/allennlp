56cb6c1ada85b27c98adc8cf0838c68c22a88c73
Gabi Stanovsky <gabis@julianm.local>
2018-09-06 16:37:27 -0700

    cleaning

--- allennlp/tests/models/open_ie_test.py
+++ allennlp/tests/models/open_ie_test.py
@@ -8,8 +8,8 @@ from allennlp.common.testing import ModelTestCase
 class OpenIETest(ModelTestCase):
     def setUp(self):
         super(OpenIETest, self).setUp()
-        self.set_up_model('tests/fixtures/openie/experiment.json',
-                          'tests/fixtures/openie/open_ie.gold_conll')
+        self.set_up_model('allennlp/tests/fixtures/openie/experiment.json',
+                          'allennlp/tests/fixtures/openie/')
 
     def test_openie_model_can_train_save_and_load(self):
         self.ensure_model_can_train_save_and_load(self.param_file)
